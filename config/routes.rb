7ffef2b463ceb54d27e81528414f80a4fb54aec9
slick <slick@slick-tabi.(none)>
2016-02-29 11:35:21 -0600

    Adds description and booking features

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :pages
 
   get '/home' => 'pages#home'
+  get '/booking' => 'pages#booking6'
+  get '/book' => 'pages#booking10'
+  get '/discounts' => 'pages#discounts'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
